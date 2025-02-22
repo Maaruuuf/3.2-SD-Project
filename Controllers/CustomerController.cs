19759a85b25e57df9e13a223ec46808b3d574b37
Maaruuuf <abc.maruf12@gmail.com>
2025-02-22 15:16:11 +0600

    customer authentication done

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Drawing;
+using Microsoft.AspNetCore.Mvc;
 using QuickBite.Models;
 
 namespace QuickBite.Controllers
@@ -6,18 +7,56 @@ namespace QuickBite.Controllers
     public class CustomerController : Controller
     {
         private myContext _context;
-       
+
         public CustomerController(myContext context)
         {
             _context = context;
-            
+
 
         }
         public IActionResult Index()
         {
             List<Category> category = _context.tbl_Category.ToList();
             ViewData["category"] = category;
+            ViewBag.checkSession = HttpContext.Session.GetString("customerSession");
             return View();
+        }
+        public IActionResult customerLogin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult customerLogin(String customerEmail, String customerPassword)
+        {
+            var customer = _context.tbl_Customer.FirstOrDefault(c => c.Customer_email == customerEmail);
+            if (customer != null && customer.Customer_password == customerPassword)
+            {
+                HttpContext.Session.SetString("customerSession",
+                   customer.Customer_id.ToString());
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.message = "Incorrect Username or Password";
+                return View();
+            }
+        }
+        public IActionResult customerRegistration()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult customerRegistration(Customer customer)
+        {
+            _context.tbl_Customer.Add(customer);
+            _context.SaveChanges();
+            return RedirectToAction("customerLogin");
+        }
+        public IActionResult customerLogout()
+        {
+            HttpContext.Session.Remove("customerSession");
+            return RedirectToAction("Index");
         }
     }
 }
